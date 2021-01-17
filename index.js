7ac83cb2a11a75909c5acf135103fe356589f0f3
Zach Berwaldt <zberwaldt@tutamail.com>
2021-01-17 15:06:07 -0500

    add yargs, start building test cli

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const yargs = require('yargs');
 const secrets = require('./secrets');
+
 const knex = require('knex')({
         client: 'pg',
         connection: {
@@ -9,6 +11,24 @@ const knex = require('knex')({
         }
 });
 
-knex.schema.hasTable('test').then(exists => {
-        console.log('there is a test table!');
-});
+yargs.scriptName('hobbyfarm')
+        .usage('$0 <cmd> [args]')
+        .command('hello [name]', 'welcome ter yargs!', (yargs) => {
+                yargs.positional('name', {
+                        type: 'string',
+                        default: 'Cambi',
+                        describe: 'the name to say hello to',
+                });
+        }, (argv) => {
+                console.log(`hello ${argv.name} welcome to yargs!`);
+        })
+        .help()
+        .argv;
+
+yargs.command('display [table]', 'show hobbyfarm', () => {}, async (argv) => {
+                let data = await knex.select().table('test');
+                console.log(data);
+                process.exit(0);
+        })
+        .help().
+        argv;
