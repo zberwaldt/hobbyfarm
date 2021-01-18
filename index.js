85a89e4864dbd11a0bef0d6791276f0756d23b4e
Zach Berwaldt <zberwaldt@tutamail.com>
2021-01-18 18:12:17 -0500

    create add function

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ yargs.command('display [table]', 'show hobbyfarm', (yargs) => {
                                                 console.log('no data in table');
                                                 process.exit(0);
                                         } else { 
-                                                let data = await knex.select().table(argv.table);
-                                                console.log(data);
+                                                let data = await knex.select('name', 'total').table(argv.table);
+                                                console.table(data);
                                                 process.exit(0);
                                         }
 
@@ -66,7 +66,41 @@ yargs.command('display [table]', 'show hobbyfarm', (yargs) => {
                 .argv;
 
 // add to table.
-// yargs.command('add', 
+yargs.command('add [table] [name] [count]', 'add new data to table',
+        (yargs) => {
+                yargs.positional('table', {
+                        type: 'string',
+                        describe: 'the table to add to',
+                });
+                yargs.positional('name', {
+                        type: 'string',
+                        describe: 'the name of the item',
+                });
+                yargs.positional('count', {
+                        type: 'number',
+                        describe: 'how many of item',
+                        default: 0
+                });
+        },
+        async (argv) => {
+                let tableExists = await knex.schema.hasTable(argv.table);
+                if(!tableExists) {
+                        console.log(`${argv.table} doesn't exist`);
+                        process.exit(0);
+                } else {
+                        try {
+                                console.log('insert into db');
+                                await knex.insert([{ name: argv.name, total: argv.count }]).into(argv.table);
+                                process.exit(0);
+
+                        } catch (e) {
+                        
+                                console.error(e);
+                                process.exit(0);
+
+                        }
+                }
+        })
 
 // initialize hobbyfarm.
 yargs.command('init', 'initialize hobbyfarm', () => {}, async (argv) => {
